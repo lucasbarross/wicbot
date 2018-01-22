1dcf43db77a030f6a94dfc2cab11c07894b5113b
Lucas Barros <lbam@cin.ufpe.br>
2018-01-22 20:17:01 -0300

    Add basic stuff

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var app = require('express')(),
-    mongoose = require('mongoose');
-    Discord = require('discord.js');
+    Discord = require('discord.js'),
+    routes = require("./routes.js"),
     client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('Logged');
-})
+client.on("ready", () => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+    client.user.setGame(`on ${client.guilds.size} servers`);
+});
+    
+client.on("guildCreate", guild => {
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    client.user.setGame(`on ${client.guilds.size} servers`);
+});
 
-app.get("/", function(req, res){
-    res.send("Hello!");
-})
+client.on("guildDelete", guild => {
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    client.user.setGame(`on ${client.guilds.size} servers`);
+});
 
-client.login('token');
-app.listen(3000, 'localhost');
+client.login(config.token);
+
+//EXPRESS
+app.use(routes);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express connected");
+});
+//
+
+setInterval(() => {
+    http.get(``);
+}, 280000);
---
+++ routes.js
@@ -0,0 +1,18 @@
+var express = require("express");
+    router = express.Router();
+    
+router.get("/", function(req, res){
+    res.send("Hello!");
+})
+
+router.post("/", function(req, res){
+    let user = req.body['client_id'];
+    let secret = req.body['secret'];
+    let msg = req.body['message'];
+
+    if(user == config.client.uid && secret == config.client.secret){
+        client.guilds.forEach((guild) => {
+            guild.defaultChannel.send(msg);
+        })
+    }
+})
