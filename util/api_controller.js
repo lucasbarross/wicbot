777809f5dbec326c65461a4b98b003a0229e224d
Lucas Barros <lbam@cin.ufpe.br>
2018-01-31 18:16:58 -0300

    Add getText api function

--- util/api_controller.js
+++ util/api_controller.js
@@ -1,13 +1,12 @@
 var axios = require("axios");
 var authorization;
-var header;
 axios.defaults.baseURL = process.env.API_URL;
 
 module.exports.getChampions = async (userID) => {
     try {
         await checkToken();
         let champions = await axios.get(`/api/v1/champions?user_id=${userID}`);
-        return champions;
+        return champions.data;
     } catch(err){
         console.log(err);
     }
@@ -17,7 +16,7 @@ module.exports.getPlayerStats = async (userID) => {
     try{
         await checkToken();
         let status = await axios.get(`/api/v1/status?user_id=${userID}`);
-        return status;  
+        return status.data;  
     } catch (err) {
         console.log(err);
     }
@@ -27,7 +26,7 @@ module.exports.getText = async (hash, lang) => {
     try{
         await checkToken();
         let localization = await axios.get(`/api/v1/localization?hash=${hash}&lang=${lang}`);
-        return localization;  
+        return localization.data;  
     } catch (err) {
         console.log(err);
     }
@@ -50,8 +49,7 @@ module.exports.postAnswer = async (userID, championID, correct, hinted) => {
 async function checkToken(){
     return new Promise((resolve, reject) => {
         if(authorization.created_at + authorization.expires_in >= new Date().getTime()){
-            getToken().then((response) => {
-                authorization = response
+            getToken().then(() => {
                 resolve();
             }).catch((err) => reject(err));
         } else {
@@ -61,14 +59,19 @@ async function checkToken(){
 }
 
 async function getToken(){
-    axios.post('/oauth/token', {
-        grant_type: "client_credentials",
-        client_id: process.env.UID,
-        client_secret: process.env.SECRET
+    return new Promise((resolve, reject) => {
+        axios.post('/oauth/token', {
+            grant_type: "client_credentials",
+            client_id: process.env.UID,
+            client_secret: process.env.SECRET
+        })
+        .then((response) => {
+            authorization = response.data
+            axios.defaults.headers.common['Authorization'] = "Bearer " + authorization.token;
+            resolve();
+        })
+        .catch((err) => {
+            reject(err);
+        });    
     })
-    .then((response) => {
-        authorization = response
-        axios.defaults.headers.common['Authorization'] = "Bearer " + authorization.token;
-    })
-    .catch((err) => console.log(err));    
 }
