3d9138df731f3cac5a428b910cd9929f26aa6f25
Lucas Barros <lbam@cin.ufpe.br>
2018-02-13 16:52:37 -0300

    Changed activity.

--- events/default.js
+++ events/default.js
@@ -5,7 +5,7 @@ const DBL = require("dblapi.js");
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-    client.user.setActivity(`on ${client.guilds.size} servers`);
+    client.user.setActivity(`?w help`);
     const dbl = new DBL(config.DBLTOKEN, client);
     axios
     .post(config.GUILD_WEBHOOK, 
