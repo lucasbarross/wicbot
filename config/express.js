f38dc283f73edbf6ac953332438b094e6ba45c16
Lucas Barros <lbam@cin.ufpe.br>
2018-02-12 23:28:14 -0300

    Add bodyparser

--- config/express.js
+++ config/express.js
@@ -1,6 +1,9 @@
 var app = require("express")(),
-    routes = require("./routes.js");
-
+    routes = require("./routes.js"),
+    bodyParser = require("body-parser");
+    
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(routes);
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express connected");
