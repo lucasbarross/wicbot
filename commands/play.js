fe85ca14bae733407473014c38e8e9e9b84a69b2
Lucas Barros <lbam@cin.ufpe.br>
2018-02-15 21:14:47 -0300

    Add collection.games current view

--- commands/play.js
+++ commands/play.js
@@ -4,6 +4,9 @@ module.exports.run = async (bot, message, args) => {
     if(bot.games.get(message.author.id)){
         message.channel.send("You are already playing!");
     } else {
+        console.log("CREATED A GAME INSTANCE");
+        console.log("COLLECTION.GAMES:");
+        console.log(bot.games);
         message.delete(0).catch((err) => console.log("ERROR DELETING ?w p MESSAGE " + err.message));
         let game = new Game(message);
         game.start();
