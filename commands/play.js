9f7b408c44b13ca624c3799e6405202a35c05522
Lucas Barros <lbam@cin.ufpe.br>
2018-02-15 23:30:00 -0300

    Fixed authorization token renewal

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,5 @@
 var Game = require("../models/Game.js");
+var api = require("../util/api_controller");
 
 module.exports.run = async (bot, message, args) => {
     if(bot.games.get(message.author.id)){
@@ -7,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         console.log("CREATED A GAME INSTANCE");
         console.log("COLLECTION.GAMES:");
         message.delete(0).catch((err) => console.log("ERROR DELETING ?w p MESSAGE " + err.message));
-        let game = new Game(message);
+        let game = new Game(message, bot, api);
         game.start();
     }
 }
--- util/api_controller.js
+++ util/api_controller.js
@@ -1,6 +1,5 @@
 var axios = require("axios");
 var config = require("../config/constants.js")
-var authorization;
 axios.defaults.baseURL = process.env.API_URL;
 axios.defaults.timeout = 10000;
 
@@ -66,13 +65,15 @@ module.exports.postAnswer = async (userID, championID, correct, hinted) => {
 
 async function checkToken(){
     return new Promise((resolve, reject) => {
-        if(!authorization || ((authorization.created_at + authorization.expires_in) <= new Date().getTime())){
+        if(!this.authorization || ((this.authorization.created_at + this.authorization.expires_in) <= new Date().getTime()/1000)){
             getToken().then((response) => {
-                authorization = response.data
-                axios.defaults.headers.common['Authorization'] = "Bearer " + authorization.access_token;
+                console.log("got token");
+                this.authorization = response.data
+                axios.defaults.headers.common['Authorization'] = "Bearer " + this.authorization.access_token;
                 resolve();
             }).catch((err) => reject(new Error("Couldn't get authorization token.")))
         } else {
+            console.log("auth already good");
             resolve();
         }
     })
