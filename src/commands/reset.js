5e8e234b2ef7dba9b0e4307d4487d66fdf70cd2c
Lucas Barros <lbam@cin.ufpe.br>
2019-01-27 12:48:10 -0300

    Add treatment if player tries to reset stats while playing

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -7,9 +7,16 @@ var api = require("../util/api_controller")
 module.exports.run = async (bot, message, args) => {
     try {
         let lang = message.guild.region == "brazil" ? "br" : "us"
-        await api.resetStats(message.author.id);
-        const line = await api.getText("resetText", lang);
-        await message.channel.send(message.author.toString() + ', ' + line.data.text)
+        let line;
+        
+        if(!bot.games.get(message.author.id)) {
+            await api.resetStats(message.author.id);
+            line = await api.getText("resetText", lang);
+        } else {
+            line = await api.getText("closeGameText", lang);
+        };
+
+        await message.channel.send(message.author.toString() + ', ' + line.data.text);
     } catch (err) {
         console.log( "Error while resetting stats", err);
     }
