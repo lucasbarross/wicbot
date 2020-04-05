d446262d2e7e286262678155078767297fed25e3
Lucas Barros <lbam@cin.ufpe.br>
2020-04-05 10:43:51 -0300

    fix(bot): fix sharding

--- src/events/default.js
+++ src/events/default.js
@@ -7,31 +7,16 @@ client.on("ready", () => {
   console.log(
     `Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`
   );
+
+  setInterval(() => {
+    dbl.postStats(client.guilds.size, client.shards.Id, client.shards.total);
+  }, 1800000);
+
   client.user.setActivity(`?w help`);
-  const dbl = new DBL(config.DBLTOKEN, client);
+
   axios
     .post(config.GUILD_WEBHOOK, {
       content: "Hey, I'm ready!",
-    })
-    .catch((err) => console.log("ERROR POSTING TO WEBHOOK"));
-});
-
-client.on("guildCreate", (guild) => {
-  console.log(
-    `New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`
-  );
-  axios
-    .post(config.GUILD_WEBHOOK, {
-      content: "Hey, I joined the guild " + guild.name,
-    })
-    .catch((err) => console.log("ERROR POSTING TO WEBHOOK"));
-});
-
-client.on("guildDelete", (guild) => {
-  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  axios
-    .post(config.GUILD_WEBHOOK, {
-      content: "Hey, " + guild.name + " guild removed me! :(",
     })
     .catch((err) => console.log("ERROR POSTING TO WEBHOOK"));
 });
