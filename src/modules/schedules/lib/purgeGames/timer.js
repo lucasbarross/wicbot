00d43c69181b8fbe636d2a32e8ec0004457224dc
Lucas Barros <lbam@cin.ufpe.br>
2020-04-06 23:33:43 -0300

    run purge every hour

--- src/modules/schedules/lib/purgeGames/timer.js
+++ src/modules/schedules/lib/purgeGames/timer.js
@@ -1,21 +1,21 @@
-const moment = require('moment'), CronJob = require('cron').CronJob;
+const moment = require("moment"),
+  CronJob = require("cron").CronJob;
 const Execute = require("./execute");
 
-
 module.exports = function (bot) {
-    try {
-        console.log("CRON PURGE ON");
+  try {
+    console.log("CRON PURGE ON");
 
-        let job = new CronJob({
-            cronTime: '00 00 05 * * *',
-            onTick: () => Execute(bot),
-            start: true,
-            runOnInit: true,
-            timeZone: 'America/Fortaleza'
-        });
+    let job = new CronJob({
+      cronTime: "0 * * * *",
+      onTick: () => Execute(bot),
+      start: true,
+      runOnInit: true,
+      timeZone: "America/Fortaleza",
+    });
 
-        console.log('CRON PURGE IS RUNNING? ', job.running);
-    } catch (err) {
-        console.log(err.message);
-    }
+    console.log("CRON PURGE IS RUNNING? ", job.running);
+  } catch (err) {
+    console.log(err.message);
+  }
 };
